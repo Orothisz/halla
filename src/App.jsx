c4becee11b956b52e7fc7e6bd3d2203e41f00636
Orothisz <112760300+Orothisz@users.noreply.github.com>
2025-08-15 18:03:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,44 @@ import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Admin from "./pages/Admin.jsx";
 
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import RedirectIfAuthed from "./components/RedirectIfAuthed.jsx";
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/assistance" element={<Assistance />} />
       <Route path="/legal" element={<Legal />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/admin" element={<Admin />} />
+
+      {/* If already signed in, don’t show login/signup */}
+      <Route
+        path="/login"
+        element={
+          <RedirectIfAuthed to="/admin">
+            <Login />
+          </RedirectIfAuthed>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <RedirectIfAuthed to="/admin">
+            <Signup />
+          </RedirectIfAuthed>
+        }
+      />
+
+      {/* /admin is protected */}
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        }
+      />
+
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
