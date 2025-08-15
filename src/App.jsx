a077154b000822f7df74af54764bad98d07c39c6
Orothisz <112760300+Orothisz@users.noreply.github.com>
2025-08-15 14:29:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Home.jsx';
-import Assistance from './pages/Assistance.jsx';
-import Legal from './pages/Legal.jsx';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Assistance from "./pages/Assistance.jsx";
+import Legal from "./pages/Legal.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Admin from "./pages/Admin.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/assistance" element={<Assistance />} />
-        <Route path="/legal" element={<Legal />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/assistance" element={<Assistance />} />
+      <Route path="/legal" element={<Legal />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
